7584f7f05ab70f02dd94c2e6e68de0380d4710bf
Jesús González <gopixc@gmail.com>
2012-09-18 22:37:33 -0500

    Se a creado prueba con CodeIginiter

---
+++ application/models/userModel.php
@@ -0,0 +1,30 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class usersModels extends CI_Model {
+
+	function __construct() {
+		parent::__construct();
+
+	}
+
+	function getUsers() {
+
+	}
+
+	function insertUsers() {
+		
+	}
+
+	function updateUsers() {
+		
+	}
+
+	function deleteUsers() {
+		
+	}
+
+	function getUserID() {
+		
+	}
+	
+}
