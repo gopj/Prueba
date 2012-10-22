23a0a7aadc80eaa1a714bf5223b5f932971c7a55
Jesús González - Planeación <gopixc@gmail.com>
2012-10-22 14:10:15 -0500

    Prueba

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -5,14 +5,40 @@ class Welcome extends CI_Controller {
 	function __construct() {
 		parent::__construct();
 		$this->load->model('usersModel');
-	}
 
+		$this->db = $this->load->database('default', TRUE); 
+		$this->load->library('pagination');
+		$this->load->model('modelo_paginador', 'modelo');
+		$this->load->helper(array('url'));
+	}
 
 	public function index(){
 		$data["nombre"] = "Chuy";
 		$this->load->view('welcome_message', $data);
 	}
+
+	public function index($offset='')
+	{
+		$this->load->view('vista_mensajes'); // solo cargaremos la vista
+	}
+
+	public function lista($offset='')
+	{
+		$limit = 2;
+		$total = $this->modelo->count_mensajes();
+		$data['mensajes'] = $this->modelo->list_mensajes($limit, $offset);
+		$config['base_url'] = base_url().'welcome/lista/';
+		$config['total_rows'] = $total;
+		$config['per_page'] = $limit;
+		$config['uri_segment'] = '3';
+		$this->pagination->initialize($config);
+		$data['pag_links'] = $this->pagination->create_links();
+		$data['title'] = 'Pagination';
+		$this->load->view('lista_vista', $data);
+	}
 }
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
 
 /* End of file welcome.php */
 /* Location: ./application/controllers/welcome.php */
