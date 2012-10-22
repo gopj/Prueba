317e745382d7c1bc82b536f12f578b1d6757a9ca
Jesús González - Planeación <gopixc@gmail.com>
2012-10-22 14:24:46 -0500

    commit

--- application/models/Modelo_paginador.php
+++ application/models/Modelo_paginador.php
@@ -6,6 +6,16 @@ class Modelo_paginador extends CI_Model {
 		parent::__construct();
 
 	}
-	
+	public function count_mensajes()
+	{
+		return $this->db->count_all_results('mensajes');
+	}
+
+	public function list_mensajes($limit, $offset)
+	{
+		$this->db->limit($limit, $offset);
+		$query = $this->db->get('mensajes');
+		return $query->result();
+	}
 	
 }
